1d26ed210935ff32ed717cd6473648dc3c779c1d
petinPetya <d-murm@yandex.ru>
2025-07-02 19:00:42 +0300

    Update bot.py
    Всё я спать

--- bot.py
+++ bot.py
@@ -2,15 +2,17 @@ import asyncio
 import logging
 import datetime
 import random
-import base64
-import re
+import re  # Why?
+from time import sleep
 from aiogram import Bot, Dispatcher, types
 from states import d, get_full
 from secret import token
-from db import users, profiles
+from decoder import decode_base64
+from db import users, profiles, sroki
 from aiogram.types import BotCommand
 from aiogram.filters.command import Command
 from aiogram import F
+from aiogram import Router
 from aiogram import html
 from aiogram.utils import deep_linking 
 from aiogram.filters import Command, CommandStart
@@ -18,7 +20,8 @@ from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.client.default import DefaultBotProperties
 from aiogram.utils.formatting import Text, Bold
 from aiogram.enums.parse_mode import ParseMode
-from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 sts = list(d.keys())
 logging.basicConfig(level=logging.INFO)
@@ -46,16 +49,23 @@ async def cmd_reply2(message: types.Message):
 async def cmd_reply3(message: types.Message):
     cur = random.choice(sts)
     cur1 = cur.replace("_", ".")
+    usr = message.from_user.full_name
+    srok = sroki[cur1[:-1]]
+    if (srok):
+        profiles[usr]["years"] += srok
+    else:
+        await message.answer("Вам повезло! Ваша статья утратила силу.")
+        return
+    print(cur1)
     builder = InlineKeyboardBuilder()
     builder.add(types.InlineKeyboardButton(
         text="Полный текст статьи",
         callback_data="num_" + str(cur) + "_" + str(message.from_user.id))
     )
     st_numb: int = cur1.split()[-1][:-1]
-    profiles[message.from_user.full_name]["stl"] = cur
+    profiles[usr]["stl"] = cur
     await message.answer(
-
-        f"{html.bold(html.quote(message.from_user.full_name))} приговаривается к статье {st_numb}: {d[cur].strip()}",
+        f"{html.bold(html.quote(usr))} приговаривается к статье {st_numb}: {d[cur].strip()}",
         reply_markup=builder.as_markup()
     )
 
@@ -66,7 +76,7 @@ async def cmd_start(message: types.Message):
     if (usr) not in users:
         users.add(usr)
         profiles[usr] = {"stl": None, "time": datetime.datetime(2000, 1, 1),
-                         "till_upd": datetime.timedelta(hours=8), "years": None, "invited":  0}
+                         "till_upd": datetime.timedelta(hours=8), "years": 0, "invited":  0}
     kb = [
         [types.KeyboardButton(text="Узнать статью"), types.KeyboardButton(text="В личное дело"), types.KeyboardButton(text="Донат")],
         [types.KeyboardButton(text="Реферальная программа")]
@@ -80,29 +90,30 @@ async def cmd_start(message: types.Message):
     if args: 
         ref_nm = args[0].encode('utf-8')
         print(ref_nm)
-        padding_needed = (4 - len(ref_nm) % 4) % 4
-        padded_string = ref_nm + b'=' * padding_needed
-        ref_nm = base64.b64decode(padded_string).decode('utf-8')
+        ref_nm = decode_base64(ref_nm)
+        if ref_nm is None:
+            await message.answer("Какая-то ошибка!")
+            return 0
         print(ref_nm, "addded_user")
         profiles[ref_nm]["invited"] += 1
+        if (profiles[ref_nm]["invited"] % 3 == 0):
+            cur = datetime.timedelta(hours=1)
+            profiles[ref_nm]["till_upd"] -= cur
 
     await message.answer(f"Здравствуйте, {html.bold(html.quote(message.from_user.full_name))}!", reply_markup=keyboard)
+    sleep(2)
 
-def decode_base64(ref_nm):
-  try:
-    decoded_bytes = base64.b64decode(ref_nm)
-    decoded_string = decoded_bytes.decode('utf-8')
-    return decoded_string
-  except (TypeError, ValueError) as e:
-    print(f"Ошибка при декодировании: {e}")
-    return None
 
 @dp.message(F.text.lower() == "узнать статью")
 @dp.message(F.text.lower() == "моя статья")
 @dp.message(F.text.lower() == "мая статья")
 async def state(message: types.Message):
     usr = message.from_user.full_name
-    usrtime = profiles[user]["time"]
+    try:
+        usrtime = profiles[usr]["time"]
+    except KeyError:
+        await message.answer("Сначала ввдите `/start`\!", ParseMode.MARKDOWN_V2)
+        return
     if (usrtime == 0 or datetime.datetime.now() - usrtime >= profiles[usr]["till_upd"]):
         profiles[message.from_user.full_name]["time"] = datetime.datetime.now()
         await cmd_reply3(message)
@@ -131,9 +142,9 @@ async def referral(message: types.Message):
     link = await deep_linking.create_start_link(bot, str(message.from_user.full_name), encode=True) 
     await message.answer(
         f"<i>Ваша пригласительная ссылка: </i> {link}\n{'-' * (len(link) + 56)}\n"
-        
+        f"<i>За каждых трёх (3) приглашённых пользователей, срок ожидания приговора сокращается на час!</i>\n"
         f"<i>Приглашено ползователей: </i> {profiles[message.from_user.full_name]['invited']}", parse_mode="HTML"
-        ) 
+        )
     
 @dp.message(F.text.lower() == "донат")
 async def donate(message: types.Message):
